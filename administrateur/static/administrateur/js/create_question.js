9292d02f683f69b810097edbcafc19df45f784a9
Adri <adri.bonnivard@gmail.com>
2023-03-16 10:38:34 +0100

    supp bin from git

--- administrateur/static/administrateur/js/create_question.js
+++ administrateur/static/administrateur/js/create_question.js
@@ -5,6 +5,7 @@ window.addEventListener('load', () => {
     function afficher() {
         console.log('ok')
         document.querySelector('.contact_div').style.display = "flex";
+        document.querySelector('.popup_question').style.display = "flex";
     }
 
     bouton.addEventListener('click', afficher)
