e0baf90bdc2b4a6d2ad1e51a97f9154495ab2403
HugoKD <h.cadet99@gmail.com>
2023-04-01 21:21:12 +0200

    Horloge et question synch au back + redirection url en fonction du pin

--- quizz/views.py
+++ quizz/views.py
@@ -1,41 +1,31 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
 from administrateur.models import Quizz, Question
 from quizz.models import User
 import time
 
-i=0
+@csrf_protect
 def interfaceUser(request,id):
-
+    i=0
     quizz = Quizz.objects.all().filter(id=id)[0]
+    questions_id=quizz.questions
+    list_id=questions_id.split(',') #ne pas prendre le dernier element (juste ' ')
     timer =quizz.timer
+    start = time.perf_counter()
+    end=time.perf_counter()
+    while i<len(list_id): #ne marche pas puisque pas une liste, il faut compter le nombre de virgule
+        question=Question.objects.get(id=list_id[i])
+        print(question)
+        context = {
+            "question": question.enonce,
+            'timer': timer
+        }
+        while abs(start-end)<timer:
+            end=time.perf_counter()
 
+        i+=1
 
-
-    #    start = time.perf_counter()
-    #    global i
-    #    question=Question.objects.all()
-    #    longueur=len(question)
-    #    nbrPage=longueur/4 ##on suppose qu'il y a 4*i questions = qcm
-     #    end= time.perf_counter()
-
-
-    return render(request, 'quizz/interfaceUser.html')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return render(request, 'quizz/interfaceUser.html',context)
 
 
 
