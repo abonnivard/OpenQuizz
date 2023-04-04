95040a9c860e9da4e7123d741852bea596dc8e0a
HugoKD <h.cadet99@gmail.com>
2023-04-04 15:39:05 +0200

    User back

--- administrateur/admin.py
+++ administrateur/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Question, Quizz
+from quizz.models import User
 
 admin.site.register(Question)
 admin.site.register(Quizz)
+admin.site.register(User)
 
