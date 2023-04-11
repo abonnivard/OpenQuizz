638269f8892e81215327b8fe98db4b577ef56699
adrien <adri.bonnivard@gmail.coml>
2023-04-11 14:47:59 +0200

    modify settings

--- OpenQuizz/settings/__init__.py
+++ OpenQuizz/settings/__init__.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=i6&^^9sbl3@5pj7%1(-t08g+1q#88uzkok$d&a1_wg)kmtp+_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', 'openquizz.h.minet.net']
+ALLOWED_HOSTS = ['127.0.0.1', 'openquizz.h.minet.net']
 
 
 # Application definition
