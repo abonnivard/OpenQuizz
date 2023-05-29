cace88ccba38cc327d98cd939556487eb26fabc7
HugoKD <h.cadet99@gmail.com>
2023-05-29 22:32:13 +0200

    erreur id

--- OpenQuizz/views.py
+++ OpenQuizz/views.py
@@ -26,4 +26,19 @@ def index(request):
     return render(request, 'OpenQuizz/index.html')
 
 def erreurId(request):
+    if request.method == 'POST':
+        ID= request.POST.get('id')
+        quizzs=Quizz.objects.all()
+        a=False
+        for quizz in quizzs: #améliorable
+            if str(quizz.id)==ID:
+                a=True
+                break
+        if a==False:
+            pin = Pin(id=ID)
+            pin.save()
+            return HttpResponseRedirect('/erreurId/') ##faire autre methode pour afficher l'erreur sans renvoyer sur une autre page
+        if (Quizz.objects.all().get(id=ID).onGame!=1):
+            return HttpResponseRedirect('/erreurId/')
+        return HttpResponseRedirect('/waitingpageUser0/'+'id='+ID+"/_") # choisi son pseudo, pour l'instant je renvoie directre sur la page, a voir comment on peut avoir
     return render(request, 'OpenQuizz/erreurId.html')
